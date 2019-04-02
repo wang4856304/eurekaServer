569456cd00822646aa2ce4bfcc9cf3539aa8425f
王军 <jun.wang@irootech.com>
2019-04-02 14:50:52 +0800

    eureka优化

--- src/main/java/com/wj/EurekaServerApplication.java
+++ src/main/java/com/wj/EurekaServerApplication.java
@@ -2,10 +2,12 @@ package com.wj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, LiquibaseAutoConfiguration.class})
 @EnableEurekaServer
 public class EurekaServerApplication {
 
